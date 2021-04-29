56d84727ccaff77a17c864221dd94be6eaf05a08
Alec <agrace072@protonmail.com>
2021-04-29 08:58:14 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/********************************************************************
+ * Filename: main.cpp
+ * Author: Alec Grace
+ * Description:
+********************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    fstream game_skater;
+    game_skater.open("game_skater_stats.csv", ios::in);
+
+
+    return 0;
+}
